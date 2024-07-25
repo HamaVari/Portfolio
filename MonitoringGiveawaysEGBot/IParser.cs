87a5b14117cdfb5c29431657db9bf00f31be3336
HamaVari <105328348+HamaVari@users.noreply.github.com>
2024-07-25 21:50:39 +0300

    Add files via upload

---
+++ MonitoringGiveawaysEGBot/IParser.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using OpenQA.Selenium.Chrome;
+using MonitoringGiveawaysEGBot.Data;
+
+namespace MonitoringGiveawaysEGBot
+{
+    public interface IParser
+    {
+        public void CheckWebsiteForChanges(string url);
+        public DateTime? ConvertUtcToKyivTimeZone(DateTime? time);
+        public Game? GetGameData(JObject? jsonObj, int index);
+        public string? GetGameTrailer(string? storeLink, int index);
+        public List<string>? GetVideoAudioFragmentLinks(string? storeLink);
+        public void Verify18Plus(ChromeDriver driver);
+        public void SearchAVFragmentsInTabs(ChromeDriver driver, List<string>? xhrUrls);
+    }
+}
---
+++ MonitoringGiveawaysEGBot/Program.cs
@@ -0,0 +1,28 @@
+﻿using Telegram.Bot;
+
+namespace MonitoringGiveawaysEGBot
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            string url = "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=ru&country=UA&allowCountries=UA";
+            var parser = new Parser();
+
+            var bot = new TgBot();
+            var botToken = //"Ваш_токен_бота";
+            var optionsBot = new TelegramBotClientOptions(botToken, baseUrl: "http://localhost:8081");
+            var botClient = new TelegramBotClient(optionsBot);
+
+            long chatId = -1001957493617;
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "Data", "GamesData", "games_data.json").Replace('\\', Path.DirectorySeparatorChar);
+
+            while (true)
+            {
+                parser.CheckWebsiteForChanges(url);
+                await bot.CheckFileAsync(botClient, chatId, filePath);
+                await Task.Delay(TimeSpan.FromDays(1));
+            }
+        }
+    }
+}
